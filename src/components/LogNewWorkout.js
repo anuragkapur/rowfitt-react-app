aa0cabd140ab24d3c9013d76d29f70ebe98ef1f2
Anurag Kapur <anurag@beancrunch.com>
2018-07-28 00:30:44 +0100

    Add static log new workout page

--- src/components/LogNewWorkout.js
+++ src/components/LogNewWorkout.js
@@ -3,10 +3,19 @@ import React from 'react';
 class LogNewWorkout extends React.Component {
   render() {
     return(
-      <div>
-        <br></br><br></br><br></br>
-        <h2>Log new workout</h2>
-      </div>
+      <main role="main" className="container rowfitt-body-main-below-navbar">
+        <h1 className="jumbotron-heading">Log a New Workout</h1>
+        <div className="row">
+          <div className="col">
+            <button type="button" className="btn btn-primary btn-lg btn-block">Single Distance</button>
+            <button type="button" className="btn btn-primary btn-lg btn-block">Single Time</button>
+            <button type="button" className="btn btn-primary btn-lg btn-block">Fixed Distance Intervals</button>
+            <button type="button" className="btn btn-primary btn-lg btn-block">Fixed Time Intervals</button>
+            <button type="button" className="btn btn-primary btn-lg btn-block">Variable Distance Intervals</button>
+            <button type="button" className="btn btn-primary btn-lg btn-block">Variable Time Intervals</button>
+          </div>
+        </div>
+      </main>
     );
   }
 }
