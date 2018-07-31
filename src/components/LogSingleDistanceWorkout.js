1a141c904b972a9b254ca43a384d626a630979f9
Anurag Kapur <anurag@beancrunch.com>
2018-07-31 12:02:50 +0100

    Add LogSingleDistanceWorkout placeholder

---
+++ src/components/LogSingleDistanceWorkout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class LogSingleDistanceWorkout extends React.Component {
+  render() {
+    return(
+      <div>
+        <br></br><br></br><br></br><br></br>
+        <h2>Log Single Distance Workout</h2>
+      </div>
+    );
+  }
+}
+
+export default LogSingleDistanceWorkout;
