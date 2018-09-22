3e5611857b0148b07f1b207bd1eb594b904f089f
Anurag Kapur <anurag@beancrunch.com>
2018-09-22 21:18:16 +0100

    Use correct url variable

--- src/components/LogSingleDistanceWorkout.js
+++ src/components/LogSingleDistanceWorkout.js
@@ -38,7 +38,7 @@ class LogSingleDistanceWorkout extends React.Component {
 
   saveWorkout = async () => {
     this.setState({ saving: true, statusMessage: "Saving..." });
-    const {status} = await axios.post(process.env.SAVE_WORKOUT_URL, this.state.workout);
+    const {status} = await axios.post(process.env.REACT_APP_SAVE_WORKOUT_URL, this.state.workout);
     var statusMessage = "Workout saved successfully!";
     if (status !== 201) {
       statusMessage = "Something went wrong, please try again!"
